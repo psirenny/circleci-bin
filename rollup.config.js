51a767aa9ddf51a56664f90a870b8bca537b7eb1
psirenny <djtorres0@gmail.com>
2018-09-15 21:12:51 -0400

    chore: make external modules explicit

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,14 @@ import rollupPluginBabel from 'rollup-plugin-babel';
 import rollupPluginHashbang from 'rollup-plugin-hashbang';
 
 export default {
+  external: [
+    'decompress',
+    'fs-extra',
+    'node-fetch',
+    'node-uname',
+    'path',
+    'tempy',
+  ],
   output: {
     format: ['cjs'],
   },
