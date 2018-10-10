0907edda3d3c8df06de0ee79b4e85cb0149ec4d7
psirenny <djtorres0@gmail.com>
2018-10-10 06:45:50 -0400

    feat: add circleci version to source

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@
 export { default as fetchReleases } from './code/modules/fetch-releases';
 export { default as installBinary } from './code/modules/install-binary';
 export { default as updateVersion } from './code/modules/update-version';
+export { default as version } from '../circleci-version';
